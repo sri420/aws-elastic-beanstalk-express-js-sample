0f38dc5ce58189272b99fd111d3cbc9850350298
Ubuntu <ubuntu@ip-172-31-24-137.ap-south-1.compute.internal>
2021-10-05 09:36:55 +0000

    changs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Chennai!'));
+app.get('/', (req, res) => res.send('Hello Madras!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
